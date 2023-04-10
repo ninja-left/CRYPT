fe909922f7b7931d778f2ca25b8fe086940bcc31
Ninja Right <s1lntlwd@proton.me>
2023-04-10 10:45:27 +0000

    Update and rename art.py to design.py

--- modules/design.py
+++ modules/design.py
@@ -51,20 +51,36 @@ def about(c1: str, c2: str, c3: str, c4: str):
 {c1}                  ╚════╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦════╝
 {c1}                       ╚╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╝{wte}
                       CRYPT, Encryption/Decryption Tool
-                      Copyright (C) 2022  N1nj4 R8
-
+                      GPL3 Copyright (C) 2022  N1nj4 R8
           CRYPT is free software: you can redistribute it and/or modify it
           under the terms of the GNU General Public License as published by
           the Free Software Foundation, either version 3 of the License, or
           any later version.
-
           CRYPT is distributed in the hope that it will be useful, but
           WITHOUT ANY WARRANTY; without even the implied warranty of
           MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
           GNU General Public License for more details.
-
           You should have received a copy of the GNU General Public License
           along with CRYPT.  If not, see <https://www.gnu.org/licenses/>.
+          
+                 TheAlgorithms/Python/ciphers <https://github.com/TheAlgorithms/Python>
+                 MIT Copyright (c) 2016-2022  TheAlgorithms and contributors
+          Permission is hereby granted, free of charge, to any person obtaining
+          a copy of this software and associated documentation files (the "Sof-
+          tware"), to deal in the Software without restriction, including with-
+          out limitation the rights to use, copy, modify, merge, publish, distri-
+          bute, sublicense, and/or sell copies of the Software, and to permit 
+          persons to whom the Software is furnished to do so, subject to the 
+          following conditions:
+          The above copyright notice and this permission notice shall be included
+          in all copies or substantial portions of the Software.
+          THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+          OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+          FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+          THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+          LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+          FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+          IN THE SOFTWARE.
 """
 
 
